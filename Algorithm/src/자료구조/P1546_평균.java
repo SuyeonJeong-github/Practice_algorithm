11dc9cd36d9fbd3d783eb208db6754eda9d0fa02
SuyeonJeong-github <success9413@naver.com>
2023-04-16 20:36:00 +0900

    Do It! 1,2번 문제

---
+++ Algorithm/src/자료구조/P1546_평균.java
@@ -0,0 +1,35 @@
+package 자료구조;
+
+import java.util.Scanner;
+
+public class P1546_평균 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+//		int[] A = new int[N];
+//		
+//		for(int i = 0; i < A.length; i++) {
+//			A[i] = sc.nextInt();
+//		}
+		
+		long sum = 0;
+		long max = 0;
+		
+//		for(int i = 0; i < A.length; i++) {
+//			if(A[i] > max) max = A[i];
+//			sum += A[i];
+//		}
+		
+		for(int i = 0; i < N; i++) {
+			int temp = sc.nextInt();
+			if(temp > max) max = temp;
+			sum += temp;
+		}
+		
+		// 한 과목과 관련된 수식을 총합한 후 관련된 수식으ㅏ로 변환해 로직이 간단해짐
+		System.out.println(sum * 100.0 / max / N);
+	}
+
+}
